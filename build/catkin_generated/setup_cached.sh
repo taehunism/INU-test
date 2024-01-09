17cbc6988ba613a3c95ef241e44de4630dab07f5
Kimtaehun <ktaeh524@gmail.com>
2024-01-09 02:33:17 +0000

    init upload

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/taehun/INU-test/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/taehun/INU-test/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/taehun/INU-test/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/taehun/INU-test/src:$ROS_PACKAGE_PATH"
